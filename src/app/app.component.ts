e31cf88db73350be8b5ff609831d3e0bb273f862
nktam <66352936+nktam@users.noreply.github.com>
2024-02-29 17:20:59 +0100

    add rxjs & angular materials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {OrdenarListaComponent} from './ordenar-lista/ordenar-lista.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OrdenarListaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'F1proyect';
+  title='F1proyect';
 }
---
+++ src/app/ordenar-lista/ordenar-lista.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-ordenar-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './ordenar-lista.component.html',
+  styleUrl: './ordenar-lista.component.css'
+})
+export class OrdenarListaComponent {
+  nombre="eneka";
+  logeado=false;
+}

9b29d2ae7ccfd598a69ed9297978af194c62a28d
eneka <eneka.tamayo@gmail.com>
2024-03-04 12:28:03 +0100

    copiar apuesta ok

--- src/app/components/apuesta/apuesta.component.ts
+++ src/app/components/apuesta/apuesta.component.ts
@@ -3,21 +3,26 @@ import {ApuestaService} from "../../services/apuesta.service";
 import {Subscription} from 'rxjs';
 import {Apuesta} from "../../interfaces/apuesta";
 import apuestaInfo from '../../utils/apuesta.json';
+import {Clipboard} from '@angular/cdk/clipboard';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-apuesta',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './apuesta.component.html',
   styleUrl: './apuesta.component.css'
 })
 export class ApuestaComponent {
 
   apuesta: Apuesta=apuestaInfo;
-  muestraEnPantalla: string="";
+  apuestaEnPantalla: string="";
   private apuestaSubscription: Subscription|undefined;
 
-  constructor(private apuestaService: ApuestaService) { }
+  constructor(private apuestaService: ApuestaService, private clipboard: Clipboard) { }
+
 
   async ngOnInit(): Promise<void> {
     this.apuestaSubscription=this.apuestaService.apuesta$.subscribe(v => this.apuesta=v);
@@ -29,22 +34,26 @@ export class ApuestaComponent {
   }
 
   private modificarApuestaEnPantalla(apuesta: Apuesta) {
-    this.muestraEnPantalla=`
-    Clasi</br>
-    1- ${apuesta.clasificacion[1].nombre}</br>
-    2- ${apuesta.clasificacion[2].nombre}</br>
-    3- ${apuesta.clasificacion[3].nombre}</br>
-    </br>
-    Carrera</br>
-    1- ${apuesta.carrera[1].nombre}</br>
-    2- ${apuesta.carrera[2].nombre}</br>
-    3- ${apuesta.carrera[3].nombre}</br>
-    </br>
-    Alonso ${apuesta.posAlonso}</br>
-    </br>
-    Sainz ${apuesta.posSainz}</br>
-    </br>
-    Equipo ${apuesta.equipo}</br>`;
+    this.apuestaEnPantalla=`Clasi
+1- ${apuesta.clasificacion[1].nombre} 
+2- ${apuesta.clasificacion[2].nombre} 
+3- ${apuesta.clasificacion[3].nombre} 
+     
+Carrera 
+1- ${apuesta.carrera[1].nombre} 
+2- ${apuesta.carrera[2].nombre} 
+3- ${apuesta.carrera[3].nombre} 
+     
+Alonso ${apuesta.posAlonso} 
+     
+Sainz ${apuesta.posSainz} 
+     
+Equipo ${apuesta.equipo}
+`;
+  }
+
+  copiar() {
+    this.clipboard.copy(this.apuestaEnPantalla);
   }
 
 }

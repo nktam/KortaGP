143b3bca756f5ee3f045d7d2a0184cbeb2bbdba0
ENEKO TAMAYO <scztazae@adm.ehu.es>
2024-03-12 08:20:14 +0100

    add capacitor androd ios

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import {CapacitorConfig} from '@capacitor/cli';
+
+const config: CapacitorConfig={
+  appId: 'eus.f1.kortagp',
+  appName: 'F1proyect',
+  webDir: 'dist/f1proyect/browser',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
